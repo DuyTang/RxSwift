d36d4a1321ff88f0146782991f2a5a3ce455a336
duytang <duytangdhv@gmail.com>
2020-08-18 10:39:33 +0700

    Init project

---
+++ TestCICD/TestCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCICD
+//
+//  Created by Avansoft on 8/18/20.
+//  Copyright © 2020 DiiDii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
